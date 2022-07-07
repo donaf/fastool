205bc46705bd6dcd538ecd4d4b47c86ac46dc315
donaf <2420639180@qq.com>
2022-07-08 07:25:44 +0800

    feat: 创建tree工具类

---
+++ src/tools/tree.ts
@@ -0,0 +1,43 @@
+/**
+ * @func generateTree
+ * @param  {}
+ * @return {}
+ * @desc   递归生成树形结构
+ * @example
+ */
+export function generateTree() { }
+
+/**
+ * @func forEachTreeNodes
+ * @param  {}
+ * @return {}
+ * @desc   遍历树节点
+ * @example
+ */
+export function forEachTreeNodes() { }
+
+/**
+ * @func  findParentNode
+ * @param  {}
+ * @return {}
+ * @desc   追溯父节点
+ * @example
+ */
+export function findParentNode() { }
+
+/**
+ * @func  findChildrenNodesAll
+ * @param  {}
+ * @return {}
+ * @desc   寻找所有子节点
+ * @example
+ */
+
+/**
+ * @func generateTreeWithPid
+ * @param  {}
+ * @return {}
+ * @desc   根据pid生成树形结构
+ * @example
+ */
+export function generateTreeWithPid() { }
